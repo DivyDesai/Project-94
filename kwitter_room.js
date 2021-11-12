d6008f2e8a04d427ae3a170d2e45c3d224a54abb
DivyDesai <84001756+DivyDesai@users.noreply.github.com>
2021-11-12 23:07:19 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBVDgE5x10dMhffZo3YcdKr2vZZsJZGmMA",
+    authDomain: "project-kwitter-c49fc.firebaseapp.com",
+    databaseURL: "https://project-kwitter-c49fc-default-rtdb.firebaseio.com",
+    projectId: "project-kwitter-c49fc",
+    storageBucket: "project-kwitter-c49fc.appspot.com",
+    messagingSenderId: "214655818540",
+    appId: "1:214655818540:web:29a30087ecc416f17d2a89"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
